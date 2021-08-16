fb17c4e393a98c970e0ca4abcdb3fc591e48cdb7
unknown <jakemillerdev@gmail.com>
2021-08-15 21:12:33 -0700

    Moved templates

--- templates/jsx.jsx
+++ templates/jsx.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function NAME_PLACEHOLDER() {
     return (
         <div>
-            
+            CODE_PLACEHOLDER
         </div>
     )
 }
---
+++ templates/tsx.tsx
@@ -0,0 +1,10 @@
+//  @ts-ignore
+import React from 'react'
+
+export default function NAME_PLACEHOLDER() {
+    return (
+        <div>
+            CODE_PLACEHOLDER
+        </div>
+    )
+}
