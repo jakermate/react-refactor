6dcc25fb93d76becea5d38734cc6beba96bd1d1d
unknown <jakemillerdev@gmail.com>
2021-08-18 13:59:23 -0700

    Tests.

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -4,6 +4,7 @@ import * as assert from 'assert';
 // as well as import your extension to test it
 import * as vscode from 'vscode';
 // import * as myExtension from '../../extension';
+import * as ext from '../../extension'
 
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
@@ -12,4 +13,12 @@ suite('Extension Test Suite', () => {
 		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
 		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
 	});
+
+	test('JSXParsing Test', () => {
+		assert.strictEqual(ext.parseJSX('<header></header>'), '<header></header>')
+		assert.strictEqual(ext.parseJSX('<header><img /></header>'), '<header><img /></header>')
+		assert.strictEqual(ext.parseJSX('<header><img /></header><p>this is a paragraph</p>'), '<div><header><img /></header><p>this is a paragraph</p></div>') 
+ 
+
+	})
 });
